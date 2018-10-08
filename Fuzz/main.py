e02cdb38e73183c36425294edcdc953435d0e655
saykim0727 <saykim0727@kaist.ac.kr>
2018-10-08 02:31:54 -0700

    Fuzzer init

---
+++ Fuzz/main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import argparse
+
+class Main:
+	def __init__(self):
+		parser = argparse.ArgumentParser(description='FrankenFuzzer')
+		args = parser.parse_args()
+		
+	
+if __name__ == "__main__":
+	fuzzer = Main()
