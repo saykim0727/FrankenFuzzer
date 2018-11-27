abd31a1ae2be99049e3cedda15611fd2691b250e
saykim0727 <saykim0727@naver.com>
2018-11-27 01:09:16 -0800

    indent clean up

--- Docker/FUZZ/fuzzer.py
+++ Docker/FUZZ/fuzzer.py
@@ -7,102 +7,102 @@ import argparse
 import sys
 
 class Fuzzer:
-	target = "/FUZZ/"
-	def __init__(self,argv):	
-		self.fuzz = argv[1]
-		self.dumb = argv[2]
-		self.indir = argv[3]
-		self.outdir = argv[4] + "/" + self.fuzz
-		self.type = argv[5]
-		self.target = self.target +  argv[6]
+    target = "/FUZZ/"
+    def __init__(self,argv):    
+        self.fuzz = argv[1]
+        self.dumb = argv[2]
+        self.indir = argv[3]
+        self.outdir = argv[4] + "/" + self.fuzz
+        self.type = argv[5]
+        self.target = self.target +  argv[6]
 
-	def runFuzzer(self):
-		os.system("mkdir /FUZZ/share/core/%s" % (self.fuzz))
-		while True:
-			if self.fuzz=="afl-fuzz":
-				afl = AFL(self.dumb,self.indir,self.outdir,self.target,self.type)
-			elif self.fuzz=="radamsa":
-				radamsa = RADAMSA(self.indir,self.outdir,self.target,self.fuzz,self.type)
-			elif self.fuzz=="honggfuzz":
-				honggfuzz = HONGGFUZZ(self.indir,self.outdir,self.target,self.type)
-			else:
-				printf("Unknown fuzzer")
+    def runFuzzer(self):
+        os.system("mkdir /FUZZ/share/core/%s" % (self.fuzz))
+        while True:
+            if self.fuzz=="afl-fuzz":
+                afl = AFL(self.dumb,self.indir,self.outdir,self.target,self.type)
+            elif self.fuzz=="radamsa":
+                radamsa = RADAMSA(self.indir,self.outdir,self.target,self.fuzz,self.type)
+            elif self.fuzz=="honggfuzz":
+                honggfuzz = HONGGFUZZ(self.indir,self.outdir,self.target,self.type)
+            else:
+                printf("Unknown fuzzer")
 
 class AFL:
-	path = "/FUZZ/mod/afl/afl-fuzz"
-	gcc_path = "/FUZZ/mod/afl/afl-gcc"
-	def __init__(self, dumb,indir,outdir,target,t):	
-		'''
-		if dumb=="False":
-			cmd = "%s -fno-stack-protector -o %s %s" %(self.gcc_path,target,target+".c")
-			afl_gcc = subprocess(cmd,shell=True)
-		'''
-		self.run(dumb,indir,outdir,target,t)
-	
-	def run(self, dumb,indir,outdir,target,t):	
-		cmd = ""
-		if dumb=="False":
-			cmd = "%s -i %s -o %s %s" % (self.path,indir,outdir,target)
-		else:
-			cmd = "%s -i %s -o %s -Q %s" % (self.path,indir,outdir,target)	
-		if t=="True": #file
-			cmd = cmd + " @@"
-		AFL_proc = subprocess.Popen(cmd,shell=True)
-		# run monitor module to run a target with afl_result for core dump
-		AFL_proc.wait()
-		# if afl is terminated, remove seed because the seed cause crash
+    path = "/FUZZ/mod/afl/afl-fuzz"
+    gcc_path = "/FUZZ/mod/afl/afl-gcc"
+    def __init__(self, dumb,indir,outdir,target,t):    
+        '''
+        if dumb=="False":
+            cmd = "%s -fno-stack-protector -o %s %s" %(self.gcc_path,target,target+".c")
+            afl_gcc = subprocess(cmd,shell=True)
+        '''
+        self.run(dumb,indir,outdir,target,t)
+    
+    def run(self, dumb,indir,outdir,target,t):    
+        cmd = ""
+        if dumb=="False":
+            cmd = "%s -i %s -o %s %s" % (self.path,indir,outdir,target)
+        else:
+            cmd = "%s -i %s -o %s -Q %s" % (self.path,indir,outdir,target)    
+        if t=="True": #file
+            cmd = cmd + " @@"
+        AFL_proc = subprocess.Popen(cmd,shell=True)
+        # run monitor module to run a target with afl_result for core dump
+        AFL_proc.wait()
+        # if afl is terminated, remove seed because the seed cause crash
 
-	# for afl-gcc with source code
-	def compile(self):
-		pass
+    # for afl-gcc with source code
+    def compile(self):
+        pass
 
 class RADAMSA:
-	path = "/FUZZ/mod/radamsa"
-	def __init__(self,indir,outdir,target,radamsa,_type):	
-		self.run(indir,outdir,target,radamsa,_type)
-	
-	def run(self,indir,outdir,target,radamsa,_type):
-		file_list = os.listdir(indir)
-		seed_name = "radamsa." + str(time.time())
-		cmd = "cat %s | %s -o %s" % (indir+"/"+file_list[0], self.path,"/FUZZ/mod/"+seed_name)
-		radamsa_proc = subprocess.Popen(cmd,shell=True)
-		radamsa_proc.wait()
-		if _type=="False":
-			cmd = "%s < %s" %(target, "/FUZZ/mod/"+seed_name)
-		else:
-			cmd = "%s %s" %(target, "/FUZZ/mod/"+seed_name)
-		radamsa_proc = subprocess.Popen(cmd,shell=True)
-		radamsa_proc.wait()
-		monitor = CoreMonitor(seed_name,radamsa)	
-		
+    path = "/FUZZ/mod/radamsa"
+    def __init__(self,indir,outdir,target,radamsa,_type):    
+        self.run(indir,outdir,target,radamsa,_type)
+    
+    def run(self,indir,outdir,target,radamsa,_type):
+        file_list = os.listdir(indir)
+        seed_name = "radamsa." + str(time.time())
+        cmd = "cat %s | %s -o %s" % (indir+"/"+file_list[0], self.path,"/FUZZ/mod/"+seed_name)
+        radamsa_proc = subprocess.Popen(cmd,shell=True)
+        radamsa_proc.wait()
+        if _type=="False":
+            cmd = "%s < %s" %(target, "/FUZZ/mod/"+seed_name)
+        else:
+            cmd = "%s %s" %(target, "/FUZZ/mod/"+seed_name)
+        radamsa_proc = subprocess.Popen(cmd,shell=True)
+        radamsa_proc.wait()
+        monitor = CoreMonitor(seed_name,radamsa)    
+        
 class HONGGFUZZ:
-	path = "/FUZZ/mod/honggfuzz/honggfuzz"
-	def __init__(self,indir,outdir,target,_type):	
-		self.run(indir,outdir,target,_type)
+    path = "/FUZZ/mod/honggfuzz/honggfuzz"
+    def __init__(self,indir,outdir,target,_type):    
+        self.run(indir,outdir,target,_type)
 
-	def run(self,indir,outdir,target,_type):
-		cmd=""
-		if _type=="False":
-			cmd = "%s -n1 -u -f %s -W %s -s -- %s" % (self.path,indir,outdir,target)
-		else:
-			cmd = "%s -n1 -u -f %s -W %s -- %s ___FILE___" % (self.path,indir,outdir,target)
-		hong_proc = subprocess.Popen(cmd,shell=True)	
-		hong_proc.wait()
-#		os.system("rm /FUZZ/share/seed/00000000000000000000000000000000.00000001.honggfuzz.cov")
-		
+    def run(self,indir,outdir,target,_type):
+        cmd=""
+        if _type=="False":
+            cmd = "%s -n1 -u -f %s -W %s -s -- %s" % (self.path,indir,outdir,target)
+        else:
+            cmd = "%s -n1 -u -f %s -W %s -- %s ___FILE___" % (self.path,indir,outdir,target)
+        hong_proc = subprocess.Popen(cmd,shell=True)    
+        hong_proc.wait()
+#        os.system("rm /FUZZ/share/seed/00000000000000000000000000000000.00000001.honggfuzz.cov")
+        
 class CoreMonitor(): #for radamsa, hodor
-	def __init__(self,seed,fuzzer):	
-		import shutil
-		from shutil import move
-		if fuzzer == "radamsa":
-			for filename in os.listdir("/TEMP"):
-				move("/TEMP/%s" % (filename),"/FUZZ/share/core_file/%s" %(filename)) 
-				shutil.copyfile("/FUZZ/mod/"+seed,"/FUZZ/share/core/radamsa/"+seed)
+    def __init__(self,seed,fuzzer):    
+        import shutil
+        from shutil import move
+        if fuzzer == "radamsa":
+            for filename in os.listdir("/TEMP"):
+                move("/TEMP/%s" % (filename),"/FUZZ/share/core_file/%s" %(filename)) 
+                shutil.copyfile("/FUZZ/mod/"+seed,"/FUZZ/share/core/radamsa/"+seed)
 
 if  __name__== "__main__":
-	argv=[]
-	for i in sys.argv:
-		argv.append(i)
-	while True:	
-		fuzzer = Fuzzer(argv)
-		fuzzer.runFuzzer()
+    argv=[]
+    for i in sys.argv:
+        argv.append(i)
+    while True:    
+        fuzzer = Fuzzer(argv)
+        fuzzer.runFuzzer()
--- Docker/FUZZ/main.py
+++ Docker/FUZZ/main.py
@@ -6,53 +6,56 @@ import time
 import subprocess
 
 class Main:
-	fuzz_list = ["afl-fuzz","radamsa","honggfuzz"]
-	def __init__(self):
-		parser = argparse.ArgumentParser(description='FrankenFuzzer')
-		parser.add_argument('-i', type=str, default="/FUZZ/share/seed",help='an integer for the accumulator')
-		parser.add_argument('-o', type=str, default="/FUZZ/share/core",help='an integer for the accumulator')
-		parser.add_argument('-d', type=str, default="False", help='an integer for the accumulator')#for afl without afl-gcc
-		parser.add_argument('-f',type=str,default="True",help="input type is file or ")
-		parser.add_argument('t',type=str,help="aaa")
+    fuzz_list = ["afl-fuzz","radamsa","honggfuzz"]
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='FrankenFuzzer')
+        parser.add_argument('-i', type=str, default="/FUZZ/share/seed",help='an integer for the accumulator')
+        parser.add_argument('-o', type=str, default="/FUZZ/share/core",help='an integer for the accumulator')
+        parser.add_argument('-d', type=str, default="False", help='an integer for the accumulator')#for afl without afl-gcc
+        parser.add_argument('-f',type=str,default="True",help="input type is file or ")
+        parser.add_argument("-crash",type=str,default="../share/core_file/",help="core_file_dir")
+        parser.add_argument('t',type=str,help="aaa")
 
-		#binary or text seed for hodor
-		self.args = parser.parse_args()
+        #binary or text seed for hodor
+        self.args = parser.parse_args()
 
-	def run(self):
-		for i in self.fuzz_list: 
-#			if i == "honggfuzz" or i == "hodor" or i=="radamsa":
-#				continue
-			docker = Docker()
-			docker.runDocker(self.args,i)
-		time.sleep(1) #time for running all fuzzer
-		#deduplication module is here
-		#Also the module has function that execute target with afl testcase to get a coredump
-		#and remove all dup
-		
+    def run(self):
+        for i in self.fuzz_list: 
+#            if i == "honggfuzz" or i == "hodor" or i=="radamsa":
+#                continue
+            docker = Docker()
+            docker.runDocker(self.args,i)
+        time.sleep(1) #time for running all fuzzer
+
+        while (True):
+            subprocess.call("python dedup.py %s %s") % (self.args.crash, self.args.t)
+            time.sleep(5)
+        #deduplication module is here
+        #Also the module has function that execute target with afl testcase to get a coredump
+        #and remove all dup
+        
 class Docker:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def runDocker(self,args,fuzzer):
-		os.chdir("../")
-		subprocess.call("./run_docker.sh f-%s %s %s %s %s/%s %s %s" % (fuzzer, fuzzer,args.d,args.i,args.o,fuzzer,args.f,args.t),shell=True)
-		os.chdir("./FUZZ")
+    def runDocker(self,args,fuzzer):
+        os.chdir("../")
+        subprocess.call("./run_docker.sh f-%s %s %s %s %s/%s %s %s" % (fuzzer, fuzzer,args.d,args.i,args.o,fuzzer,args.f,args.t),shell=True)
+        os.chdir("./FUZZ")
 
 
 class Monitor:
-	def __init__(self):
-		self.indir = ConfigParsor("MONITOR_IN")
-		self.outdir = ConfigParsor("MONITOR_OUT")
+    def __init__(self):
+        self.indir = ConfigParsor("MONITOR_IN")
+        self.outdir = ConfigParsor("MONITOR_OUT")
 
-	
-			
 
 if __name__ == "__main__":
-	os.system("rm -rf ../share/core")
-	os.system("mkdir ../share/core")
-	os.system("rm -rf ../share/core_file")
-	os.system("mkdir ../share/core_file")
-	os.system("rm -f ../share/log/*")
-	main = Main()
-	main.run()
+    os.system("rm -rf ../share/core")
+    os.system("mkdir ../share/core")
+    os.system("rm -rf ../share/core_file")
+    os.system("mkdir ../share/core_file")
+    os.system("rm -f ../share/log/*")
+    main = Main()
+    main.run()
